11dbbd0acc28f4153c758f96267bd30451935d82
Abhishek Dulat <abhishek11906997dulat@gmail.com>
2023-12-28 18:31:50 +0530

    latest

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
 /*
@@ -28,6 +29,7 @@ const MONGO_URL = process.env.MONGO_URL;
 
 Connection(MONGO_URL);
 
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use("/user", userRegister);
